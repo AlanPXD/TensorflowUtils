c64f31780b643a454b60dced468f6b0c6ea4b22e
Alan Peterson <apeterson056@febe.pads.ufrj.br>
2024-03-06 23:11:13 -0300

    Renomeando e agrupando funções de arquivos diferentes

--- Compoments.py
+++ Compoments.py
@@ -1,8 +1,110 @@
-from abc import ABC, abstractmethod, abstractproperty
+import ast
+from pandas import read_csv
+from abc import ABC, abstractmethod
+from genericpath import isdir
+from os import makedirs
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any
 from tensorflow.keras.models import Model, model_from_json
-from TensorflowUtils.KerasModelInfo import *
+from TensorflowUtils.ModelDataGeters import *
+
+class DataHandle ():
+
+    def __init__(self, 
+                 trainings_file_path = "/home/apeterson056/AutoEncoder/codigoGitHub/IC-AutoEncoder/Logs/Trainings_data.csv",
+                 models_file_path = "/home/apeterson056/AutoEncoder/codigoGitHub/IC-AutoEncoder/Logs/models_data.csv",
+                 models_columns: list = [
+                    "model_name",
+                    "total_params",
+                    "params_distribution",
+                    "total_layers",
+                    "total_treinable_layers",
+                    "total_paths",
+                    "total_kernel_regularizers",
+                    "total_bias_regularizers",
+                    "total_activity_regularizers",
+                    "kernel_regularizers_distribution",
+                    "bias_regularizers_distribution",
+                    "activity_regularizers_distribution",
+                    "kernel_regularizers_L1_distribution",
+                    "bias_regularizers_L1_distribution",
+                    "activity_regularizers_L1_distribution",
+                    "kernel_regularizers_L2_distribution",
+                    "bias_regularizers_L2_distribution",
+                    "activity_regularizers_L2_distribution",
+                    "initializer_count",
+                    "dropout_count",
+                    "dropout_distribution",
+                    "batch_normalization_count",
+                    "batch_normalization_distribution"
+                 ],
+                 training_columns: list = [
+                    "training_idx",
+                    "model_name",
+                    "optimizer_args",
+                    "loss_args",
+                    "data_set_info",
+                    "best_results",
+                    "date_time",
+                    "training_time_seconds"
+                    "training_time",
+                    "n_epochs"
+                 ]) -> None:
+        
+        self.trainings_file_path = trainings_file_path
+        self.models_file_path = models_file_path
+
+        self.training_dataframe = read_csv(trainings_file_path, converters= dict.fromkeys(training_columns, self.literal_converter))
+        self.models_dataframe = read_csv(models_file_path, converters= dict.fromkeys(models_columns, self.literal_converter))
+
+    def literal_converter (self, data) -> any:
+        try: 
+            return ast.literal_eval(str(data))
+        except SyntaxError: # probably str
+            return data
+        
+
+class DirManagerABC (ABC):
+    @abstractmethod
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    @abstractmethod
+    def make_all_dirs(self) -> None:
+        pass
+        
+
+
+class KerasDirManager (DirManagerABC):
+
+    def __init__ (self,
+                logs_dir_name:str,
+                models_csv_name:str,
+                trainings_csv_name:str,
+                model_name: str, 
+                dataset_name: str,
+                training_idx: int,
+                ) -> None:
+
+        self.base_dir = f"{logs_dir_name}/{dataset_name}/{model_name}/{training_idx}"
+        self.model_save_best_training_pathname = self.base_dir + "/model_best_training_checkpoint"
+        self.model_save_best_validation_pathname = self.base_dir + "/model_best_validation_checkpoint"
+        
+        self.metric_means_pathname = f"{logs_dir_name}/{dataset_name}/{model_name}/{training_idx}/metrics_epoch_means.csv"
+
+        self.models_table_pathname = f"{logs_dir_name}/{models_csv_name}.csv"
+        self.trainings_table_pathname = f"{logs_dir_name}/{trainings_csv_name}.csv"
+        self.samples_path = f"{logs_dir_name}/{dataset_name}/{model_name}/{training_idx}/"
+
+        self.make_all_dirs()
+        
+    def make_all_dirs (self) -> None:
+
+        if not isdir(self.base_dir):
+            makedirs(self.base_dir)
+
+
 
 
 
@@ -152,6 +254,3 @@ class KerasNeuralNetData(NeuralNetDataABC):
             Model to be used in trainings, predicts...
         """
         return self.model
-
-
-
